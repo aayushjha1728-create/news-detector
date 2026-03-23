782ae2d730f9c1af32346e4893919857bd53a73e
aayushjha1728-create <aayushjha1728@gmail.com>
2026-03-23 22:38:02 +0530

    Remove commented link and clean up code formatting

--- news.py
+++ news.py
@@ -1,4 +1,4 @@
-//https://drive.google.com/drive/folders/1osrck82G_gDLl-aqsI-9Jm-DzmcBAMHl?usp=drive_link(files)
+https://drive.google.com/drive/folders/1osrck82G_gDLl-aqsI-9Jm-DzmcBAMHl?usp=drive_link(files)
 
 
 from flask import Flask, render_template, request
